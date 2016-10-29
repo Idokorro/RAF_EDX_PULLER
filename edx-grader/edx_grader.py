3777cba815a77313a94280c42f4632286410d109
Idokorro <igor.ciganovic@gmail.com>
2016-10-29 20:54:45 +0200

    Added partial grading

--- edx-grader/edx_grader.py
+++ edx-grader/edx_grader.py
@@ -55,9 +55,10 @@ def get_queue_length(queue_name, xqueue_session):
 
     return True, response
 
+
 def grade_submission(content_header, content_body):
     from grader.grader import test_code
-    from grader.working_directory import WorkingDirectory
+
     grader_payload = json.loads(content_body['grader_payload'])
     grader_url = urllib.request.urlopen("http://" + grader_payload['tester'])
 
@@ -66,42 +67,27 @@ def grade_submission(content_header, content_body):
     submission_code = content_body['student_response']
 
     result = test_code(language, grader_code, submission_code)
-    print(result)
 
-    return True, 1, "<p>Correct</p>"
+    return calculate_response(result, grader_payload)
 
 
-# def run_external_grader(content_header, content_body):
-#     grader_payload = json.loads(content_body['grader_payload'])
-#     path = os.path.join(SUBMISSIONS_ROOT, content_header['submission_key'])
-#     os.makedirs(path)
-#
-#     # GRADER
-#     grader_url = urllib.request.urlopen("http://" + grader_payload['tester'])
-#     grader_file_path = os.path.join(path, 'Grader.py')
-#     with open(grader_file_path, 'wb') as f:
-#         f.write(grader_url.read())
-#     f.close()
-#     grader_url.close()
-#
-#     # SUBMISSION
-#     student_response_file_path = os.path.join(path, 'Submission' + EXTENSIONS[grader_payload['lang']])
-#     with open(student_response_file_path, 'w') as f:
-#         f.write(content_body['student_response'])
-#     f.close()
-#
-#     # EXECUTE
-#     results = grader.test_solution(grader_payload['lang'], grader_file_path, student_response_file_path)
-#
-#     return calculate_correction(results)
-#
-#
-# def calculate_correction(results):
-#     for result in results["results"]:
-#         if not result["success"]:
-#             return False, 0, "<p><emph>" + result["error_message"] + "</emph></p>" + "<p>" + result[
-#                 "traceback"] + "</p>"
-#     return True, 1, "<p><emph>Uspešno ste rešili problem. Čestitamo!</emph></p>"
+def calculate_response(results, payload):
+    msg = ""
+    score = 0
+    num_tests = 0
+    for result in results["results"]:
+        num_tests += 1
+        if result["success"]:
+            score += 1
+            msg += "<p class='alert alert-success' style='color: green'>" + result["description"] + "</p>"
+        else:
+            msg += "<p class='alert alert-danger' style='color: red'>" + result["error_message"] + "</p>"
+
+    if payload['partial'] and score > 0 or score == num_tests:
+        points = (float(score) / num_tests) * payload['num_points']
+        return True, points, msg
+    else:
+        return False, 0, msg
 
 
 try:
